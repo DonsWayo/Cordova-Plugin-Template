d8c7cd50dd3d750c115860e06e05b492326f8937
Unknown <boby940913@gmail.com>
2018-07-06 11:52:34 +0800

    Update variables' defination

--- www/MyCordovaPlugin.js
+++ www/MyCordovaPlugin.js
@@ -1,9 +1,9 @@
 
-var exec = require('cordova/exec');
+const exec = require('cordova/exec');
 
-var PLUGIN_NAME = 'MyCordovaPlugin';
+const PLUGIN_NAME = 'MyCordovaPlugin';
 
-var MyCordovaPlugin = {
+let MyCordovaPlugin = {
   echo: function(args, successCallBack , failureCallback) {
     exec(successCallBack, failureCallback, PLUGIN_NAME, 'echo', [args]);
   }
