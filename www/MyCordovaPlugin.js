1dd093aed304a1fcc4c77d61505f7593c062ec0c
boby913 <boby940913@gmail.com>
2019-03-26 13:47:18 +0800

    Revert "Update variables' defination"
    This reverts commit d8c7cd50dd3d750c115860e06e05b492326f8937.

--- www/MyCordovaPlugin.js
+++ www/MyCordovaPlugin.js
@@ -1,9 +1,9 @@
 
-const exec = require('cordova/exec');
+var exec = require('cordova/exec');
 
-const PLUGIN_NAME = 'MyCordovaPlugin';
+var PLUGIN_NAME = 'MyCordovaPlugin';
 
-let MyCordovaPlugin = {
+var MyCordovaPlugin = {
   echo: function(args, successCallBack , failureCallback) {
     exec(successCallBack, failureCallback, PLUGIN_NAME, 'echo', [args]);
   }
